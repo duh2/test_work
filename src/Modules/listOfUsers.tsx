6a4080f6b2f362c73c69506c4f25012a34637b2b
duh2 <hristabudda2@gmail.com>
2021-03-25 02:05:13 +0700

    added data resolving

--- src/Modules/listOfUsers.tsx
+++ src/Modules/listOfUsers.tsx
@@ -1,7 +1,15 @@
 import {Component} from "react";
 import {States} from "../Redux/store";
+
 const mapStateToProps = (store:States)=>{
     return {userID:store.value_id}
+}
+interface User {
+    login:string
+}
+interface ListOfUsersInterface {
+isLoaded:boolean;
+data:Array<User>
 }
 
 export class ListOfUsers extends Component<any, any>{
@@ -16,9 +24,18 @@ export class ListOfUsers extends Component<any, any>{
             xhr.open("GET", `https://api.github.com/users?q=${idValue}`, true)
         }
         xhr.send()
-        xhr.onreadystatechange =()=>{
-            if (xhr.readyState!==4){
-                return {false}
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState!==4) {
+                return {isLoaded:false}
+            }
+            if (xhr.status!==200) {
+                console.log(xhr.status +":"+xhr.statusText);
+            } else {
+                let data = JSON.parse(xhr.responseText)
+                this.setState({
+                    data: getAllUsers? data : data.items,
+                    isLoaded:true
+                });
             }
         }
     }
