ee9d9ee895ef63d0bf00fcef61cb43d97b110275
duh2 <hristabudda2@gmail.com>
2021-03-25 01:29:01 +0700

    started to add header

---
+++ src/Modules/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Header :React.FC=() => {
+    return(
+        <div>
+            <label>User ID:
+                <input type='text'/>
+            </label>
+        </div>
+    )
+}
