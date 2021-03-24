967a74f6468ae20437d1d3e070dc563fac9cfd4c
duh2 <hristabudda2@gmail.com>
2021-03-25 01:40:29 +0700

    started to add handling of enter key pressed

--- src/Modules/Header.tsx
+++ src/Modules/Header.tsx
@@ -1,15 +1,18 @@
 import React, {useState} from "react";
 import '../ProjectCSS.css'
+import {useDispatch} from "react-redux";
 
 export const Header :React.FC=() => {
     const [selectedID, setSelectedID] = useState('')
+    const dispatch = useDispatch()
     const handleChangeID =(event: React.ChangeEvent<HTMLInputElement>)=>{
         setSelectedID(event.target.value)
     }
+    const handleEnterPressed
     return(
         <div className='header'>
             <label>User ID:
-                <input type='text' onChange={handleChangeID}/>
+                <input type='text' onChange={(event)=>{handleChangeID(event)}} onKeyPress={(event)=>{handleEnterPressed(event)}}/>
             </label>
         </div>
     )
