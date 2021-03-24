5e7824e83a319161b4b36699c5f62a5b4cc19c95
duh2 <hristabudda2@gmail.com>
2021-03-25 01:22:38 +0700

    adding reducer

--- src/Redux/reducer.ts
+++ src/Redux/reducer.ts
@@ -1,3 +1,6 @@
 export const reducer = (state, action)=>{
-    
+    switch (action.type) {
+        case "ACTION_ID": return {...state, value_id: action.value};
+        default: return state;
+    }
 }
