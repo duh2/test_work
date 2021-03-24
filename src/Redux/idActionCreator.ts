4e93ae2242aad5989fb21618fb4b943c8673dcd2
duh2 <hristabudda2@gmail.com>
2021-03-25 01:18:44 +0700

    added store action creator, started to implement reducer

---
+++ src/Redux/idActionCreator.ts
@@ -0,0 +1,7 @@
+export function actionIdEntered(value:string|undefined) {
+    return {
+        type:"ACTION_ID",
+        value:value
+    }
+
+}
---
+++ src/Redux/store.ts
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+export const initialState ={
+    value_id:''
+}
+
+
+export const store = createStore()
